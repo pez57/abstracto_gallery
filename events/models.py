9362a0b4e2b0694f9c066bf45f5b23820cb3c5c2
pez57 <perry_s@live.com>
2022-09-21 10:49:17 +0000

    Added event app and basic models

---
+++ events/models.py
@@ -0,0 +1,21 @@
+import uuid
+from django.db import models
+
+class EventCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class EventTicket(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    category = models.ForeignKey(EventCategory, on_delete=models.PROTECT, null=False)
+    artist = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField()
+    time = models.TimeField()
+    reference_number = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    
+    def __str__(self):
+        return self.name
